40bb6bc7bf020238af1b84b81e09f3fe394cb7fa
CENTURION 1 <kdtkachenko@gmail.com>
2025-05-26 23:43:28 +0300

    задача 25 операции с матрицами

---
+++ Matrix/Matrix1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class Matrix1
+    {
+        private double a11, a12, a21, a22;
+
+        // Конструктор
+        public Matrix1(double a11, double a12, double a21, double a22)
+        {
+            this.a11 = a11;
+            this.a12 = a12;
+            this.a21 = a21;
+            this.a22 = a22;
+        }
+
+
+
+        // Метод определителя
+        public double Det()
+        {
+            return a11 * a22 - a12 * a21;
+        }
+
+        // Метод транспонирования
+        public Matrix1 Transpose()
+        {
+            return new Matrix1(a11, a21, a12, a22);
+        }
+
+        // Операция сложения матриц
+        public static Matrix1 operator +(Matrix1 m1, Matrix1 m2)
+        {
+            return new Matrix1(
+                m1.a11 + m2.a11,
+                m1.a12 + m2.a12,
+                m1.a21 + m2.a21,
+                m1.a22 + m2.a22
+            );
+        }
+
+        // Операция вычитания матриц
+        public static Matrix1 operator -(Matrix1   m1, Matrix1 m2)
+        {
+            return new Matrix1(
+                m1.a11 - m2.a11,
+                m1.a12 - m2.a12,
+                m1.a21 - m2.a21,
+                m1.a22 - m2.a22
+            );
+        }
+
+        // Операция умножения: матрица * матрица
+        public static Matrix1 operator *(Matrix1 m1, Matrix1 m2)
+        {
+            return new Matrix1(
+                m1.a11 * m2.a11 + m1.a12 * m2.a21,
+                m1.a11 * m2.a12 + m1.a12 * m2.a22,
+                m1.a21 * m2.a11 + m1.a22 * m2.a21,
+                m1.a21 * m2.a12 + m1.a22 * m2.a22
+            );
+        }
+
+        // Операция умножения: матрица * число
+        public static Matrix1 operator *(Matrix1 m, double scalar)
+        {
+            return new Matrix1(
+                m.a11 * scalar, m.a12 * scalar,
+                m.a21 * scalar, m.a22 * scalar
+            );
+        }
+
+        // Метод для вывода матрицы
+        public void Print()
+        {
+            Console.WriteLine($"[{a11,6:0.##} {a12,6:0.##}]");
+            Console.WriteLine($"[{a21,6:0.##} {a22,6:0.##}]");
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,57 @@
+﻿using Matrix;
+
+class Program
+{
+    static void Main()
+    {
+        var a = InputMatrix();
+        var b = InputMatrix();
+
+        var m1 = new Matrix1(a[0], a[1], a[2], a[3]);
+        var m2 = new Matrix1(b[0], b[1], b[2], b[3]);
+
+        Console.WriteLine("Матрица m1:");
+        m1.Print();
+
+        Console.WriteLine("\nМатрица m2:");
+        m2.Print();
+
+        Console.WriteLine("\nСумма m1 + m2:");
+        (m1 + m2).Print();
+
+        Console.WriteLine("\nРазность m1 - m2:");
+        (m1 - m2).Print();
+
+        Console.WriteLine("\nПроизведение m1 * m2:");
+        (m1 * m2).Print();
+
+        Console.WriteLine("\nУмножение m1 на число 2:");
+        (m1 * 2).Print();
+
+        Console.WriteLine($"\nОпределитель m1: {m1.Det()}");
+
+        Console.WriteLine("\nТранспонированная m1:");
+        m1.Transpose().Print();
+    }
+
+    static double[] InputMatrix()
+    {
+        double[] a = new double[4];
+        while (true)
+        {
+            Console.WriteLine("Введите 4 элемента матрицы через пробел:");
+            string input = Console.ReadLine();
+            string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 4 && parts.All(p => double.TryParse(p, out _)))
+            {
+                for (int i = 0; i < 4; i++)
+                    a[i] = double.Parse(parts[i]);
+                break;
+            }
+
+            Console.WriteLine("Ошибка! Введите ровно 4 числа.");
+        }
+        return a;
+    }
+}

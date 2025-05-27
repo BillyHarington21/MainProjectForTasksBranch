75bb201ee30dde2935467f709cadb76b6d754cdb
CENTURION 1 <kdtkachenko@gmail.com>
2025-05-27 21:08:21 +0300

    задача 25 , вариант с векторами

---
+++ Vectors/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+using Vectors;
+
+namespace Vectors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание экземпляров класса Vector2
+            Vector vector1 = new Vector(2, 2);
+            Vector vector2 = new Vector(4, 6);
+                      
+            Console.WriteLine($"vector1: {vector1}");
+            Console.WriteLine($"vector2: {vector2}");
+
+            // Длины 
+            Console.WriteLine($"Длина vector1: {vector1.Length():F3}");
+            Console.WriteLine($"Длина vector2: {vector2.Length():0.000}");
+
+            // Сложение
+            Console.WriteLine($"vector1 + vector2 = {vector1 + vector2}");
+
+            // Вычитание
+            Console.WriteLine($"vector1 - vector2 = {vector1 - vector2}");
+
+            // Скалярное
+            Console.WriteLine($"vector1 * vector2 = {vector1 * vector2}");
+
+            // Умножение на число
+            Console.WriteLine($"vector1 * 5 = {vector1 * 5}");
+
+            // Деление на число
+            Console.WriteLine($"vector2 / 2 = {vector2 / 2}");
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Vectors/Vector.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vectors
+{
+    public class Vector
+    {
+        // Поля
+        public double x;
+        public double y;
+
+        // Конструктор, принимающий координаты вектора
+        public Vector(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        // Метод, возвращающий длину вектора
+        public double Length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        // Операция сложения двух векторов
+        public static Vector operator +(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x + v2.x, v1.y + v2.y);
+        }
+
+        // Операция вычитания двух векторов
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.x - v2.x, v1.y - v2.y);
+        }
+
+        // Операция скалярного умножения вектора на вектор
+        public static double operator *(Vector v1, Vector v2)
+        {
+            return v1.x * v2.x + v1.y * v2.y;
+        }
+
+        // Операция умножения на число
+        public static Vector operator *(Vector v1, double a)
+        {
+            return new Vector(v1.x * a, v1.y * a);
+        }
+
+        // Операция деления на число
+        public static Vector operator /(Vector v1, double a)
+        {
+            return new Vector(v1.x / a, v1.y / a);
+        }
+
+        // Метод для строкового представления вектора
+        public override string ToString()
+        {
+            return $"({x}; {y})";
+        }
+    }
+}
+

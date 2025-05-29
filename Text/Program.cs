3c8ecaaa0069c0e56bd7f56abfd274a1f5fb187c
CENTURION 1 <kdtkachenko@gmail.com>
2025-05-29 18:46:14 +0300

    задание 27, выбор слова из текста по частоте или по лексикографическому порядку

---
+++ Text/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите текст:");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+        foreach (string word in words)
+        {
+            if (wordCount.ContainsKey(word))
+                wordCount[word]++;
+            else
+                wordCount[word] = 1;
+        }
+
+        // Находим максимальную частотуарбуз арбуз
+        int maxCount = wordCount.Values.Max();
+
+        // Отбираем все слова с этой частотой
+        var needWords = wordCount
+            .Where(pair => pair.Value == maxCount)
+            .Select(pair => pair.Key)
+            .ToList();
+
+        // Выбираем минимальное по алфавиту
+        string result = needWords.Min();
+
+        if (needWords.Count > 1)
+        {            
+            Console.WriteLine(" Выбрано минимальное в лексикографическом порядке.");
+        }
+        else
+        {
+            Console.WriteLine(" Это единственное слово с наибольшей частотой.");
+        }
+        Console.WriteLine(result);
+    }
+}
